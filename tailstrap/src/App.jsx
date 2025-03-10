58daafd9ee8dcca4784559c4588f26dbe1d75586
poptaro <noahahn199@gmail.com>
2025-03-10 09:41:50 -0500

    week6 start init. no functionality

---
+++ tailstrap/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-6xl text-red-500'>
+        Hi
+        <button className='btn btn-primary'>CLICK ME</button>
+      </div>
+    </>
+  )
+}
+
+export default App
